b06abf220e689240b5d8282428b93eef6f53f427
skmodishashi <skmodi.shashi@gmail.com>
2026-02-14 01:34:27 +0530

    Bubble sort

---
+++ Sorting/bubblesort.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main(){ // Bubble sort->Unsorted array->ascending order
+    int arr[5]={5,4,3,2,1};
+    int n=5;
+    for(int i=0;i<n;i++){
+      printf("%d ",arr[i]);
+    }
+    printf("\n");
+    for(int i=0;i<n-1;i++){  //outer loop will stand for no. of passes
+        for(int j=0;j<=n-2;j++){  // inner loop will do the swapping
+            if(arr[j]>arr[j+1]){
+                int temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+            }
+        }
+    }
+    for(int i=0;i<n;i++){
+      printf("%d ",arr[i]);
+    }
+    return 0;
+}
