6278fddb8ff4ef153ab8cc45dba3046797260e99
skmodishashi <skmodi.shashi@gmail.com>
2026-02-16 17:13:55 +0530

    insertion sort

---
+++ Sorting/insertionsort.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main(){
+   int arr[5]={1,5,3,0,2};
+   int n=5;   
+    printf("Unsorted array : \n");               
+    for(int i=0;i<n;i++){
+      printf("%d ",arr[i]);
+    }
+    
+    for(int i=1;i<=n-1;i++){
+        int j=i;
+        while(j>=1 && arr[j]<arr[j-1]){
+            int temp = arr[j];
+            arr[j] = arr[j-1];
+            arr[j-1] = temp;
+            j--;
+                }
+    }
+    printf("\nSorted array : \n");               
+    for(int i=0;i<n;i++){
+      printf("%d ",arr[i]);
+    }
+    return 0;
+}
