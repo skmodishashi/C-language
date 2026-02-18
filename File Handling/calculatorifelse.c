12b825711a4ef47788e548184be48ece4b89210f
skmodishashi <skmodi.shashi@gmail.com>
2026-02-18 11:31:46 +0530

    Calculator

---
+++ File Handling/calculatorifelse.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main(){
+    int a;
+    scanf("%d",&a);
+    char ch; // +,-,/,* --> operator
+    scanf("%c",&ch);
+    int b;
+    scanf("%d",&b);
+    if(ch=='+')
+       printf("%d",a+b);
+    if(ch=='-')
+       printf("%d",a-b);
+    if(ch=='*')
+       printf("%d",a*b);
+    if(ch=='/')
+       printf("%f",(float)a/b);  
+
+       return 0;
+}
---
+++ File Handling/calculatorswitch.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main(){
+    int a;
+    scanf("%d",&a);
+    char ch; // +,-,/,* --> operator
+    scanf("%c",&ch);
+    int b;
+        scanf("%d",&b);
+    switch(ch){
+        case '+':
+         printf("%d",a+b);
+         break;
+    case '-':
+         printf("%d",a-b);
+         break;
+    case '*':
+         printf("%d",a*b);
+         break;
+    case '/':
+         printf("%f",(float)a/b);
+         break;
+    default:
+        printf("Invalid Operator");
+    }
+
+    return 0;
+}
