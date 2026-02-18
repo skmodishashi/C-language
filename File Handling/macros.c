c977372c5b08ff49222b70b28d470f6f6d3c226d
skmodishashi <skmodi.shashi@gmail.com>
2026-02-18 10:59:10 +0530

    macros

---
+++ File Handling/macros.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+#define PI 3.14159265359
+#define area(r) (PI*r*r)
+
+int main(){
+    printf("%f",area(10));
+}
--- File Handling/preproccesor.c
+++ File Handling/preproccesor.c
@@ -1,5 +1,21 @@
 #include<stdio.h>
+#include<math.h>
+#include<limits.h>
 int main(){
-    
+    //math.h
+    float x = sqrt(7);  // sqrt and cbrt can be used by using math.h
+    printf("%f\n",x);
+    float y = cbrt(7);
+    printf("%f\n\n",y);
+
+    //limits.h
+    int a = INT_MAX;
+    printf("%d\n",a);
+    int b = INT_MIN;
+    printf("%d\n",b);
+    long c = LONG_MAX;
+    printf("%ld\n",c);
+    long d = LONG_MIN;
+    printf("%ld\n",d);
     return  0;
 }
