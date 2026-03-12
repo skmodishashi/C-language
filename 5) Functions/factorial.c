bab72d5612ec36b777076394633f5d779761198a
skmodishashi <skmodi.shashi@gmail.com>
2026-03-12 21:05:05 +0530

    factorial

---
+++ 5) Functions/factorial.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int factorial(int n){
+    int fact = 1;
+    for (int i = 2; i <= n; i++)
+    {
+        fact = fact*i;
+    }
+    return fact;
+}
+int main(){
+    int n;
+    printf("Enter a number :");
+    scanf("%d",&n);
+    int fact = factorial(n);
+    printf("%d", fact);
+    return 0;
+}
